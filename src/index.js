38dc6b6326c878692892270104ef88606d59bad4
Orit Gegelia <o.gegelia@yandex.ru>
2024-01-07 12:33:36 +0300

    Validation, main part

--- src/index.js
+++ src/index.js
@@ -32,6 +32,10 @@ const placeLink = addFormElement.querySelector('input[name="link"]');
 const imagePopup = document.querySelector(".popup_type_image");
 const cardPopupImage = document.querySelector(".popup__image");
 
+const avatarPopup = document.querySelector('.popup_type_avatar');
+const avatarEdit = document.querySelector('.profile__image')
+const avatarFormElement = document.querySelector('form[name="edit-avatar"]')
+
 // Добавление новой карточки на страницу
 
 function addCard(card) {
@@ -53,7 +57,7 @@ initialCards.forEach((card) => {
   addCard(card, deleteCard);
 });
 
-// Слушатели открытия модалок редактирования и добавления карточки
+// Слушатели открытия модалок редактирования и добавления карточки и редактирования аватара
 
 addPopup.addEventListener("submit", () => popupHandleCloser(addPopup));
 addPopupButton.addEventListener("click", () => popupHandleOpener(addPopup));
@@ -64,6 +68,18 @@ editPopupButton.addEventListener("click", () => {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 });
+
+avatarPopup.addEventListener("submit", () => popupHandleCloser(avatarPopup));
+avatarEdit.addEventListener("click", () => popupHandleOpener(avatarPopup));
+
+// Форма редактирования аватара
+
+function handleEditAvatar(evt) {
+  evt.preventDefault();
+  avatarFormElement.reset();
+}
+
+avatarFormElement.addEventListener('submit', handleEditAvatar)
 
 // Форма редактирования профиля
 
@@ -110,3 +126,108 @@ function imagePopupOpener(evt) {
 
   document.addEventListener("keydown", escapeCloseModal);
 }
+
+// Валидация форм
+
+// Показать ошибку
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+
+  const formError = formElement.querySelector(`.${inputElement.id}-error`)
+
+  inputElement.classList.add('popup__input-error');
+  formError.textContent = errorMessage;
+  formError.classList.add('popup__input-error-text-active');
+};
+
+// Скрыть ошибку
+
+const hideInputError = (formElement, inputElement) => {
+
+  const formError = formElement.querySelector(`.${inputElement.id}-error`)
+  
+  inputElement.classList.remove('popup__input-error');
+  formError.textContent = '';
+  formError.classList.remove('popup__input-error-text-active');
+};
+
+// Проверка формы на валидность
+
+const formIsValid = (formElement, inputElement) => {
+  if (inputElement.validity.patternMismatch) {
+    inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+  } else {
+    inputElement.setCustomValidity('');
+  }
+
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+}
+
+// Навешивает слушатели на все инпуты.
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__button')
+
+  invalidButtonDisabling(inputList, buttonElement)
+  
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      formIsValid(formElement, inputElement);
+      invalidButtonDisabling(inputList, buttonElement);
+    });
+  });
+};
+
+// Блокировка кнопки и отправки формы
+
+const containInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  })
+}
+
+const invalidButtonDisabling = (inputList, buttonElement) => {
+  if (containInvalidInput(inputList)) {
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.disabled = false;
+  }
+}
+
+// Запускает валидацию всего и вся.
+
+function enableValidation() {
+
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+
+  formList.forEach((formElement) => {
+  formElement.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+  });
+
+  setEventListeners(formElement);
+});
+  }
+
+enableValidation()
+
+// API иисусе авось получится
+
+function currentUserData() {
+  fetch('https://nomoreparties.co/v1/wff-cohort-4/users/me', {
+  headers: {
+    authorization: '31536b87-2f99-402f-bf92-fda5063b463d'
+  }
+}).then(res => res.json())
+.then((result) => {
+  profileName.textContent = result.name;
+  profileJob.textContent = result.about
+})} 
+
+currentUserData()
+
